6cf063c055cb7ec6c3ebc7a5d0cf495b0ff14a47
Deep Thought <harpreetset@gmail.com>
2018-02-12 12:03:21 -0800

    Add files via upload

---
+++ travelbotconfig.js
@@ -0,0 +1,7 @@
+var travelbotconfig = {
+    "subscription_key":"cb82f033c251434590b71530d9e7aa54",
+    "app_id":"b1a73111-d2b1-4e48-b871-aa9cd5f29e85",
+    "endpoint":"https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/"+app_id,
+};
+
+module.exports=travelbotconfig;
