f7f93766c65fe2ebeaf8c79c49d470b3a6e78141
Harpreet Sethi <harpreet.sethi@wolterskluwer.com>
2018-02-12 13:18:05 -0800

    some updates made

--- flights.js
+++ flights.js
@@ -1,3 +1,88 @@
-module.exports = function (session) {
-    session.error('Flights Dialog is not implemented and is instead being used to show Bot error handling');
+var builder = require('botbuilder');
+var Store = require('./store');
+
+module.exports = [
+    // Destination
+    function (session) {
+        session.send('Welcome to the Flights finder!');
+        builder.Prompts.text(session, 'Please enter your origin');
+    },
+    function (session, results, next) {
+        session.dialogData.origin = results.response;
+        builder.Prompts.text(session, 'Please enter your destination');
+        next();
+    },
+    function (session, results, next) {
+        session.dialogData.destination = results.response;
+        session.send('Looking for hotels in %s', results.response);
+        next();
+    },
+    // Check-in
+    function (session) {
+        builder.Prompts.time(session, 'When do you want to fly?');
+    },
+    function (session, results, next) {
+        session.dialogData.checkIn = results.response.resolution.start;
+        next();
+    },
+
+    // Nights
+    function (session) {
+        builder.Prompts.number(session, 'When do you want to return?');
+    },
+    function (session, results, next) {
+        session.dialogData.nights = results.response;
+        next();
+    },
+
+    // Search...
+    function (session) {
+        var origin = session.dialogData.origin;
+        var destination = session.dialogData.destination;
+        var checkIn = new Date(session.dialogData.checkIn);
+        var checkOut = checkIn.addDays(session.dialogData.nights);
+
+        session.send(
+            'Ok. Searching for Flights from %s to %s date from %d/%d to %d/%d...',
+            origin,destination,
+            checkIn.getMonth() + 1, checkIn.getDate(),
+            checkOut.getMonth() + 1, checkOut.getDate());
+
+        // Async search
+        Store
+            .searchFlights(origin, destination, checkIn, checkOut)
+            .then(function (flights) {
+                // Results
+                session.send('I found in total %d flights for your dates:', flights.length);
+
+                var message = new builder.Message()
+                    .attachmentLayout(builder.AttachmentLayout.carousel)
+                    .attachments(flights.map(flightAsAttachment));
+
+                session.send(message);
+
+                // End
+                session.endDialog();
+            });
+    }
+];
+
+// Helpers
+function flightAsAttachment(flight) {
+    return new builder.HeroCard()
+        .title(flight.name)
+        .subtitle('%d stars. %d reviews. From $%d per passenger.', flight.rating, flight.numberOfReviews, flight.priceStarting)
+        .images([new builder.CardImage().url(flight.image)])
+        .buttons([
+            new builder.CardAction()
+                .title('More details')
+                .type('openUrl')
+                .value('https://www.bing.com/search?q=flights +to+' + encodeURIComponent(flight.location))
+        ]);
+}
+
+Date.prototype.addDays = function (days) {
+    var date = new Date(this.valueOf());
+    date.setDate(date.getDate() + days);
+    return date;
 };
--- store.js
+++ store.js
@@ -22,5 +22,27 @@ module.exports = {
             // complete promise with a timer to simulate async response
             setTimeout(function () { resolve(hotels); }, 1000);
         });
+    },
+    searchFlights: function (origin,destination, checkInDate, checkOutDate) {
+        return new Promise(function (resolve) {
+
+            // Filling the flights results manually just for demo purposes
+            var flights = [];
+            for (var i = 1; i <= 5; i++) {
+                flights.push({
+                    name: destination + ' Flight ' + i,
+                    location: destination,
+                    rating: Math.ceil(Math.random() * 5),
+                    numberOfReviews: Math.floor(Math.random() * 5000) + 1,
+                    priceStarting: Math.floor(Math.random() * 450) + 80,
+                    image: 'https://placeholdit.imgix.net/~text?txtsize=35&txt=Flight+' + i + '&w=500&h=260'
+                });
+            }
+
+            flights.sort(function (a, b) { return a.priceStarting - b.priceStarting; });
+
+            // complete promise with a timer to simulate async response
+            setTimeout(function () { resolve(flights); }, 1000);
+        });
     }
 };
